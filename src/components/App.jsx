3d04822deb262f0a77e6826e1bd17c0fab0c0664
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-05-02 13:50:43 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Container } from './Container/Container';
+import { Form } from './Form/Form';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    for (const item of this.state.contacts) {
+      if (item.name.toLowerCase() === name.toLowerCase())
+        return window.alert(`${name} is already in contacts`);
+    }
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    // PrevState = contacts; ...PrevState.contacts = ...contacts
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.addContact} />
+
+        <h2>Contacts</h2>
+
+        <Filter onChange={this.changeFilter} value={filter} />
+        <ContactsList
+          contacts={visibleContacts}
+          onClickButtonDelete={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,5 @@
+import css from './Container.module.css'
+
+export const Container = ({ children }) => (
+    <div className={css.container}>{children}</div>
+)
